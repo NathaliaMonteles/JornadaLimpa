aa2953c35dffcbd3907c52679f4ca26b7930f06c
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-07-16 00:12:36 -0300

    preparação para o deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Contato from './pages/contato/Contato';
 import Home from './pages/home/Home';
 import Sobre from './pages/sobre/Sobre';
 
+
+
 import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import { AuthProvider } from './context/AuthContext';
